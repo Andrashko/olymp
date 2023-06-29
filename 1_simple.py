493ac638294d9546035f4758bd2ed2a87dce109c
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2023-06-29 20:57:23 +0300

    todo

--- 1_simple.py
+++
@@ -1,4 +0,0 @@
-number = int(input())
-first_digit = number // 10
-second_digit = number % 10
-print(first_digit, second_digit)
--- 5_squares.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-numbers_count = [0]*101
-numbers_string = input()
-for number in numbers_string.split(" "):
-    numbers_count[int(number)] += 1
-squares_count = 0
-for count in numbers_count:
-    squares_count += count // 4
-print(squares_count)
